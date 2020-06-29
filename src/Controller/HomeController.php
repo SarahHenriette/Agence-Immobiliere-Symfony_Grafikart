0ae9f2523b8a8029f7282d6725ee703dc7e160cf
Elise Rousseau <rousseaujumelle@gmail.com>
2020-06-29 23:12:12 +0200

    projet site agence immobiliere, mise en place de la base dedonnee

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\PropertyRepository;
+
+
+class HomeController extends AbstractController{
+
+
+/**
+ * @Route("/", name="home") 
+ * */ 
+    public function index(PropertyRepository $repository)
+    {
+
+        $properties = $repository->findLatest();
+        return $this->render('pages/home.html.twig', [
+            'properties'=> $properties
+        ]);
+    }
+}
